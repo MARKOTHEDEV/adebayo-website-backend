83204a87e41dc7e840c6bdc19b67684d683093ea
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-12-08 21:29:23 -0600

    added new models to the admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import (
-    User,peopleEnrollfor,Event,Testimonial, Quotes,ResourcesTable,SkitTable
+    User,peopleEnrollfor,Event,Testimonial, Quotes,ResourcesTable,SkitTable,Contact
+    
 )
 
 admin.site.register(User)
@@ -9,3 +10,4 @@ admin.site.register(Event)
 admin.site.register(Testimonial)
 admin.site.register(ResourcesTable)
 admin.site.register(SkitTable)
+admin.site.register(Contact)
